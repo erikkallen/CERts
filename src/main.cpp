02b4318e82764ab9865de072ca4011a0447f6ac1
Erik Kallen <erik@diagnoptics.com>
2019-01-29 15:33:16 +0100

    updated to meson build system

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 #include <SDL_image.h>
 #include "renderer.h"
 #include "img_loader.h"
+#include "debug.h"
 
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 void logSDLError(std::ostream &os, const std::string &msg);
 
 int main(int argc, char **argv){
-	
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		logSDLError(std::cout, "SDL_Init");
 		return 1;
@@ -27,12 +28,12 @@ int main(int argc, char **argv){
 		logSDLError(std::cout, "CreateRenderer");
 		return 3;
 	}
-	
+
 	SDL_Texture *background = loadTexture("res/background.png", renderer);
 	SDL_Texture *image = loadTexture("res/image.png", renderer);
 	if (background == nullptr || image == nullptr)
 		return 4;
-	
+
 	SDL_RenderClear(renderer);
 
 	int bW, bH;
@@ -41,7 +42,7 @@ int main(int argc, char **argv){
 	renderTexture(background, renderer, bW, 0);
 	renderTexture(background, renderer, 0, bH);
 	renderTexture(background, renderer, bW, bH);
-	
+
 	int iW, iH;
 	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
 	int x = SCREEN_WIDTH / 2 - iW / 2;
@@ -50,7 +51,7 @@ int main(int argc, char **argv){
 
 	SDL_RenderPresent(renderer);
 	SDL_Delay(2000);
-	
+
 	SDL_DestroyTexture(background);
 	SDL_DestroyTexture(image);
 	SDL_DestroyRenderer(renderer);
